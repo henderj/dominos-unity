78b448210be73b09b93b44794cbabadb0949d7b3
Josh (ubuntu) <hendershotjoshr@gmail.com>
2021-12-13 11:40:19 -0400

    configured assemblies

--- Assets/Tests/PlayerTest.cs
+++ Assets/Tests/PlayerTest.cs
@@ -1,25 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
-using UnityEngine;
-using UnityEngine.TestTools;
 
-public class PlayerTest
+namespace Tests
 {
-    // A Test behaves as an ordinary method
-    [Test]
-    public void PlayerTestSimplePasses()
+    public class PlayerTest
     {
-        // Use the Assert class to test conditions
-    }
+        [Test]
+        public void HasDoubleSix()
+        {
+            var player = new Player("PT");
+            player.GiveDominoes(new Domino[]{new Domino(6,6)});
+            Assert.True(player.HasDoubleSix());
+        }
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator PlayerTestWithEnumeratorPasses()
-    {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
+        // // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // // `yield return null;` to skip a frame.
+        // [UnityTest]
+        // public IEnumerator PlayerTestWithEnumeratorPasses()
+        // {
+        //     // Use the Assert class to test conditions.
+        //     // Use yield to skip a frame.
+        //     yield return null;
+        // }
     }
 }
