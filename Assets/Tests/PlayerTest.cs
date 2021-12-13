1887d506432f7a542db54e5bf120e4472b7f1cb8
Josh (ubuntu) <hendershotjoshr@gmail.com>
2021-12-13 11:48:06 -0400

    more tests

--- Assets/Tests/PlayerTest.cs
+++ Assets/Tests/PlayerTest.cs
@@ -4,12 +4,33 @@ namespace Tests
 {
     public class PlayerTest
     {
+        private Player player;
+        [SetUp]
+        public void Setup()
+        {
+            player = new Player("PT");
+        }
+        
         [Test]
         public void HasDoubleSix()
         {
-            var player = new Player("PT");
-            player.GiveDominoes(new Domino[]{new Domino(6,6)});
+            player.GiveDominoes(new[]{new Domino(6,6)});
             Assert.True(player.HasDoubleSix());
+        }
+
+        [Test]
+        public void IsHandEmpty()
+        {
+            Assert.True(player.IsHandEmpty());
+        }
+
+        [Test]
+        public void PlayDoubleSix()
+        {
+            player.GiveDominoes(new[]{new Domino(6,6)});
+            var expected = new Domino(6, 6);
+            var actual = player.PlayDoubleSix();
+            Assert.AreEqual(expected, actual);
         }
 
         // // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
