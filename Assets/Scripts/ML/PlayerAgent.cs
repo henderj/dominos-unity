fa636b797f93ed6903afd14edbb77344afee171d
Josh Hendershot <35842246+henderj@users.noreply.github.com>
2022-01-21 07:37:35 -0400

    Update PlayerAgent.cs

--- Assets/Scripts/ML/PlayerAgent.cs
+++ Assets/Scripts/ML/PlayerAgent.cs
@@ -2,8 +2,29 @@ using Unity.MLAgents;
 
 namespace ML
 {
-    public class PlayerAgent : Agent
+    public class PlayerAgent : Agent, IMoveChooser
     {
+        private Move[] _currentMoves;
+        private Domino[] _currentPlayedDominoes;
+        
+        public Move Choose(Move[] moves, Domino[] playedDominoes){
+            _currentMoves = moves;
+            _currentPlayedDominoes = playedDominoes;
+            
+        }
+    
+        public override void OnEpisodeBegin(){}
+        
+        public override void CollectObservation(VectorSensor sensor){}
+        
+        public override void WriteDiscreteActionMask(IDiscreteActionMask actionMask)
+        {
+            actionMask.SetActionEnabled(branch, actionIndex, isEnabled);
+        }
+        
+        public override void OnActionRecieved(){}
+        
+        public override void Heuristic(){}
         
     }
 }
